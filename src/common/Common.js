8968591294b92ef20c0829a7d49fc21f02795d44
dofany <kimdohwan17@gmail.com>
2022-12-18 21:33:03 +0900

    김도환 | API 호출 공통 함수 부분 수정
    Former-commit-id: 8cfdfebe45a5dcc05ff34ed03819b305c7e49651

--- src/common/Common.js
+++ src/common/Common.js
@@ -4,15 +4,12 @@ import Loading from "./component/Loading/Loading";
 
 // api 호출 공통 함수
 
-export const CommonAxios = (url, data, methodType, callback) => {
-    
-    const [loading, setLoading] = useState(true);
-
+export const CommonAxios = (url, params, methodType, callback) => {
     useEffect(() => {
         axios(
             {
                 url : "/api" + url,
-                data : data,
+                params : params,
                 method : methodType,
                 contentType : "application/json; charset=utf=8",
                 enctype : "multipart/form-data" // form태그 데이터 전송
@@ -22,13 +19,7 @@ export const CommonAxios = (url, data, methodType, callback) => {
         }).catch(function(error){
             JSON.stringify(error);
         }).finally(function(){
-            setLoading(false);
+
         });
     }, [] );
-    
-
-    // 로딩바 리턴
-    return (
-        <div>{loading && <Loading />}</div>
-    );
 }
--- src/user/component/UserList.js
+++ src/user/component/UserList.js
@@ -1,35 +1,30 @@
 import React, {useState} from 'react';
 import { CommonAxios } from '../../common/Common';
-
+import Loading from "../../common/component/Loading/Loading";
 
 function UserList() {
 
     const [users, setUsers] = useState([]);
+    // 우선적으로 loading을 true로 함
+    const [loading, setLoading] = useState(true);
 
+    // api 호출 예시
+    // params : input 값(object 타입으로 넣어야함)
+    const params = {email : "dekaron12@naver.com"}
+    CommonAxios("/user/userFind11", params ,"get", getData);
 
-    // useEffect(() => {
-    //     axios(
-    //         {
-    //             url : "/api/user/getUserList",
-    //             method : 'get',
-    //             contentType : "application/json; charset=utf=8",
-    //             enctype : "multipart/form-data" // form태그 데이터 전송
-    //         }
-    //     ).then(function(response){
-    //         getData(response.data);
-    //     }).catch(function(error){
-    //         JSON.stringify(error);
-    //     });
-    // }, [])
-
-    CommonAxios("/user/getUserList", "" ,"get", getData);
-
+    // 콜백함수(우선적 테스트용 => api 호출이 안될시 무한으로 로딩바가 생성됨)
+    // 지속적 개선할 예정
     function getData(data) {
         setUsers(data);
+        setLoading(false);
     }
-    
+
     return(
         <div>
+            <div>
+                {loading ? <Loading /> : null}
+            </div>
             <h2 className="text-center">PlanE API Test</h2>
             <div className="row">
                 <table className="table table-striped table-bordered">
@@ -41,9 +36,9 @@ function UserList() {
                     </thead>
                     <tbody className="text-center">
                         {users.map(item =>  (
-                          <tr key = {item.userId}>
-                              <td> {item.userId} </td>
-                              <td> {item.userName} </td>
+                          <tr key = {item.email}>
+                              <td> {item.email} </td>
+                              <td> {item.userNm} </td>
                           </tr>
                         )
                     )}
