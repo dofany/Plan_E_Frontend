8442e0f69bc7050249c94f152100728e914c12ba
dofany <kimdohwan17@gmail.com>
2023-04-20 14:51:21 +0900

    김도환 | 메뉴바 주석해제
    Former-commit-id: 15741808fc454b402ef15a138d898b2747f87948

--- src/layouts/FullLayout/Sidebar/data.js
+++ src/layouts/FullLayout/Sidebar/data.js
@@ -1,12 +1,12 @@
 import {
-  // DashboardOutlined,
-  // AddToPhotosOutlined,
-  // AspectRatioOutlined,
-  // AssignmentTurnedInOutlined,
-  // AlbumOutlined,
-  // SwitchCameraOutlined,
-  // SwitchLeftOutlined,
-  // DescriptionOutlined,
+  DashboardOutlined,
+  AddToPhotosOutlined,
+  AspectRatioOutlined,
+  AssignmentTurnedInOutlined,
+  AlbumOutlined,
+  SwitchCameraOutlined,
+  SwitchLeftOutlined,
+  DescriptionOutlined,
   AutoAwesomeMosaicOutlined,
   CalendarTodayOutlined,
 } from "@material-ui/icons/";
@@ -22,46 +22,46 @@ const Menuitems = [
     icon: AutoAwesomeMosaicOutlined,
     href: "/tables/basic-table",
   },
-  // {
-  //   title: "Dashboard",
-  //   icon: DashboardOutlined,
-  //   href: "/dashboards/dashboard",
-  // },
-  // {
-  //   title: "Autocomplete",
-  //   icon: AddToPhotosOutlined,
-  //   href: "/form-elements/autocomplete",
-  // },
-  // {
-  //   title: "Buttons",
-  //   icon: AspectRatioOutlined,
-  //   href: "/form-elements/button",
-  // },
-  // {
-  //   title: "Checkbox",
-  //   icon: AssignmentTurnedInOutlined,
-  //   href: "/form-elements/checkbox",
-  // },
-  // {
-  //   title: "Radio",
-  //   icon: AlbumOutlined,
-  //   href: "/form-elements/radio",
-  // },
-  // {
-  //   title: "Slider",
-  //   icon: SwitchCameraOutlined,
-  //   href: "/form-elements/slider",
-  // },
-  // {
-  //   title: "Switch",
-  //   icon: SwitchLeftOutlined,
-  //   href: "/form-elements/switch",
-  // },
-  // {
-  //   title: "Form",
-  //   icon: DescriptionOutlined,
-  //   href: "/form-layouts/form-layouts",
-  // },
+  {
+    title: "Dashboard",
+    icon: DashboardOutlined,
+    href: "/dashboards/dashboard",
+  },
+  {
+    title: "Autocomplete",
+    icon: AddToPhotosOutlined,
+    href: "/form-elements/autocomplete",
+  },
+  {
+    title: "Buttons",
+    icon: AspectRatioOutlined,
+    href: "/form-elements/button",
+  },
+  {
+    title: "Checkbox",
+    icon: AssignmentTurnedInOutlined,
+    href: "/form-elements/checkbox",
+  },
+  {
+    title: "Radio",
+    icon: AlbumOutlined,
+    href: "/form-elements/radio",
+  },
+  {
+    title: "Slider",
+    icon: SwitchCameraOutlined,
+    href: "/form-elements/slider",
+  },
+  {
+    title: "Switch",
+    icon: SwitchLeftOutlined,
+    href: "/form-elements/switch",
+  },
+  {
+    title: "Form",
+    icon: DescriptionOutlined,
+    href: "/form-layouts/form-layouts",
+  },
 ];
 
 export default Menuitems;
